2f25994d014f6da15204de125a5b75c811dade7d
khm31 <khm31@Kwon>
2025-12-06 23:18:12 +0900

    2025-12-06 방송 수업 토요일

--- java1/src/week_broadcast1206/ex_inter.java
+++ java1/src/week_broadcast1206/ex_inter.java
@@ -1,5 +1,13 @@
 package week_broadcast1206;
 
-public interface ex_inter {
+import java.util.*;
 
+public interface ex_inter { //사수가 이렇게 만들어 놨으면 무조건 오버라이드 해야한다.
+	//이렇게 써놓으면 무조건 사용하세요가 된다.
+	public <T> Object user(T data);
+	public <T> ArrayList<T> box(T data); //String, Integer, Double => 다 사용가능하다.
+	public <T> LinkedList<T> box2(T data);
+	public <T> List<T> box3(T data);
+	public <T> Map<T, T> box4(); //이렇게도 작성가능
+	
 }
--- java1/src/week_broadcast1206/week5.java
+++ java1/src/week_broadcast1206/week5.java
@@ -1,5 +1,6 @@
 package week_broadcast1206;
 
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
